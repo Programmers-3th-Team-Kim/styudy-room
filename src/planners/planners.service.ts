685c6085383ed6639314d57c4bba632cb1a4bd1e
II-122 <76612998+II-122@users.noreply.github.com>
2024-09-26 18:35:07 +0900

    refactor: 할 일 수정 코드 업데이트
    refactor: 할 일 수정 코드 개선

--- src/planners/planners.service.ts
+++ src/planners/planners.service.ts
@@ -19,6 +19,7 @@ export class PlannersService {
         ...plannerDto,
         userId: new Types.ObjectId(userId),
       });
+      console.log('root 데이터 생성 성공');
 
       const savedPlan = await newPlanQuery.save();
       console.log(newPlanQuery.save());
@@ -28,23 +29,27 @@ export class PlannersService {
       const repeatWeeks = plannerDto.repeatWeeks;
 
       if (repeatDays) {
+        console.log('반복 요일이 존재합니다');
+        console.log(`${repeatWeeks}주간 반복`);
         for (const day of repeatDays) {
           for (let i: number = 0; i < repeatWeeks; i++) {
             const selectedDay = this.mappingDays[day];
-            const today = new Date().getDay();
+            const today = new Date();
+            const todayDate = today.getDay();
+            const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
-            if (
-              today != selectedDay &&
-              (selectedDay == 0 || today < selectedDay)
-            ) {
-              const planId: Types.ObjectId = savedPlan._id as Types.ObjectId;
-              const dateOffset = ((selectedDay - today + 7) % 7) + i * 7;
+            const planId: Types.ObjectId = savedPlan._id as Types.ObjectId;
+            const dateOffset = ((selectedDay - todayDate + 7) % 7) + i * 7;
 
-              const selectedDate = new Date();
-              selectedDate.setDate(date.getDate() + dateOffset);
-              const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
+            const selectedDate = new Date();
+            selectedDate.setDate(date.getDate() + dateOffset);
+            const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
 
+            if (todayString < planDate) {
               createDataNum += 1;
+              console.log(
+                `데이터 생성, 할 일 날짜: ${planDate}, 오늘: ${todayString}`
+              );
               await this.createPlanCascade(
                 userId,
                 planId,
@@ -145,10 +150,24 @@ export class PlannersService {
     const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
     if (parentObjectId) {
-      rootId = parentObjectId;
+      rootId = new Types.ObjectId(parentObjectId);
+      console.log(rootId);
+    }
+
+    // 할 일의 날짜가 오늘
+    if (todayString == date) {
+      console.log('업데이트...');
+      await this.plannerModel
+        .findByIdAndUpdate(
+          rootId,
+          { ...plannerDto, userId: new Types.ObjectId(userId) },
+          { new: true }
+        )
+        .exec();
     }
 
     // 오늘 이후 날짜의 데이터 삭제
+    console.log(todayString);
     await this.deletePlanCascade(rootId, todayString);
 
     // 오늘 이후 날짜의 데이터 재생성
@@ -160,28 +179,28 @@ export class PlannersService {
     let recreateDataNum: number = 0;
 
     if (repeatDays) {
+      console.log(`${repeatDays}요일 ${repeatWeeks}주간 반복`);
       for (const day of repeatDays) {
         for (let i: number = 0; i < repeatWeeks; i++) {
           const selectedDay = this.mappingDays[day];
           const rootDateNum = rootDate.getDay();
 
-          if (selectedDay == 0 || rootDateNum < selectedDay) {
-            const planId: Types.ObjectId = rootData._id as Types.ObjectId;
-            const dateOffset = ((selectedDay - rootDateNum + 7) % 7) + i * 7;
+          const planId: Types.ObjectId = rootData._id as Types.ObjectId;
+          const dateOffset = ((selectedDay - rootDateNum + 7) % 7) + i * 7;
 
-            const selectedDate = new Date();
-            selectedDate.setDate(rootDate.getDate() + dateOffset);
-            const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
+          const selectedDate = new Date();
+          selectedDate.setDate(rootDate.getDate() + dateOffset);
+          const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
 
-            if (today < selectedDate) {
-              recreateDataNum += 1;
-              await this.createPlanCascade(
-                userId,
-                planId,
-                planDate,
-                updatePlannerDto
-              );
-            }
+          if (todayString < planDate) {
+            console.log(`오늘 ${todayString} , 할 일 날짜 ${planDate}`);
+            recreateDataNum += 1;
+            await this.createPlanCascade(
+              userId,
+              planId,
+              planDate,
+              updatePlannerDto
+            );
           }
         }
       }
