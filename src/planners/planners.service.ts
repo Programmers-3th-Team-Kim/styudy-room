55cceabef9e7ff54339cffeef01ca0a5c6a1f643
II-122 <76612998+II-122@users.noreply.github.com>
2024-10-04 17:48:09 +0900

    fix: 할 일 수정 버그 fix
    fix: 할 일 수정 버그 fix

--- src/planners/planners.service.ts
+++ src/planners/planners.service.ts
@@ -150,63 +150,67 @@ export class PlannersService {
     const today = new Date();
     const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
-    if (parentObjectId) {
+    if (!parentObjectId) {
+      await this.plannerModel
+        .findByIdAndUpdate(rootId, plannerDto, { new: true })
+        .exec();
+    } else {
       rootId = new Types.ObjectId(parentObjectId);
       console.log(rootId);
-    }
 
-    // 할 일의 날짜가 오늘
-    if (todayString == date) {
-      console.log('업데이트...');
-      await this.plannerModel
-        .findByIdAndUpdate(
-          rootId,
-          { ...plannerDto, userId: new Types.ObjectId(userId) },
-          { new: true }
-        )
-        .exec();
-    }
+      // 할 일의 날짜가 오늘
+      if (todayString === date) {
+        console.log('업데이트...');
+        await this.plannerModel
+          .findByIdAndUpdate(
+            rootId,
+            { ...plannerDto, userId: new Types.ObjectId(userId) },
+            { new: true }
+          )
+          .exec();
+      }
 
-    // 오늘 이후 날짜의 데이터 삭제
-    console.log(todayString);
-    await this.deletePlanCascade(rootId, todayString);
+      // 오늘 이후 날짜의 데이터 삭제
+      console.log(rootId);
+      await this.deletePlanCascade(rootId, todayString);
 
-    // 오늘 이후 날짜의 데이터 재생성
-    const rootData = await this.plannerModel.findById(rootId).exec();
-    const rootDate = new Date(rootData.date);
+      // 오늘 이후 날짜의 데이터 재생성
+      const rootData = await this.plannerModel.findById(rootId).exec();
+      const rootDate = new Date(rootData.date);
 
-    const repeatDays = updatePlannerDto.repeatDays;
-    const repeatWeeks = updatePlannerDto.repeatWeeks;
-    let recreateDataNum: number = 0;
+      const repeatDays = updatePlannerDto.repeatDays;
+      const repeatWeeks = updatePlannerDto.repeatWeeks;
+      let recreateDataNum: number = 0;
 
-    if (repeatDays) {
-      console.log(`${repeatDays}요일 ${repeatWeeks}주간 반복`);
-      for (const day of repeatDays) {
-        for (let i: number = 0; i < repeatWeeks; i++) {
-          const selectedDay = this.mappingDays[day];
-          const rootDateNum = rootDate.getDay();
+      if (repeatDays) {
+        console.log(`${repeatDays}요일 ${repeatWeeks}주간 반복`);
+        for (const day of repeatDays) {
+          for (let i: number = 0; i < repeatWeeks; i++) {
+            const selectedDay = this.mappingDays[day];
+            const rootDateNum = rootDate.getDay();
 
-          const planId: Types.ObjectId = rootData._id as Types.ObjectId;
-          const dateOffset = ((selectedDay - rootDateNum + 7) % 7) + i * 7;
+            const planId: Types.ObjectId = rootData._id as Types.ObjectId;
+            const dateOffset = ((selectedDay - rootDateNum + 7) % 7) + i * 7;
 
-          const selectedDate = new Date();
-          selectedDate.setDate(rootDate.getDate() + dateOffset);
-          const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
+            const selectedDate = new Date();
+            selectedDate.setDate(rootDate.getDate() + dateOffset);
+            const planDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
 
-          if (todayString < planDate) {
-            console.log(`오늘 ${todayString} , 할 일 날짜 ${planDate}`);
-            recreateDataNum += 1;
-            await this.createPlanCascade(
-              userId,
-              planId,
-              planDate,
-              updatePlannerDto
-            );
+            if (todayString < planDate) {
+              console.log(`오늘 ${todayString} , 할 일 날짜 ${planDate}`);
+              recreateDataNum += 1;
+              await this.createPlanCascade(
+                userId,
+                planId,
+                planDate,
+                updatePlannerDto
+              );
+            }
           }
         }
       }
+      console.log(`${recreateDataNum} 개의 데이터가 재생성되었습니다.`);
     }
-    console.log(`${recreateDataNum} 개의 데이터가 재생성되었습니다.`);
   }
 
   async deletePlan(userId: string, plannerId: string): Promise<Planner> {
